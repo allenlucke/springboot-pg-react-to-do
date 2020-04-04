a52b7e89203ed597f434f65b6a611d6321435df2
Allen Lucke <allenlucke@gmail.com>
2020-04-04 18:14:09 -0500

    inital codebase

---
+++ src/test/java/com/java_todo/springbootpgreacttodo/SpringbootPgReactToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_todo.springbootpgreacttodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPgReactToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
