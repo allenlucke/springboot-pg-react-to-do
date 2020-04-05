91298e306d7263cdf3e7e3e5e45a5efca2499ef2
Allen Lucke <allenlucke@gmail.com>
2020-04-05 17:29:42 -0500

    started react FE file structure

---
+++ src/main/resources/static/scripts/components/App/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+import './App.css';
+
+class App extends Component {
+
+    }
+
+export default connect()(App);
