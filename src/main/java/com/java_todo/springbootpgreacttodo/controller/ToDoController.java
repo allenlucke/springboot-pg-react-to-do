e7658d0fcc6b0378aa307e84cd949e63c40e126c
Allen Lucke <allenlucke@gmail.com>
2020-04-06 16:07:33 -0500

    removed most node and js files to seperate project folder

--- src/main/java/com/java_todo/springbootpgreacttodo/controller/ToDoController.java
+++ src/main/java/com/java_todo/springbootpgreacttodo/controller/ToDoController.java
@@ -1,3 +1,5 @@
+
+
 package com.java_todo.springbootpgreacttodo.controller;
 import com.java_todo.springbootpgreacttodo.exception.ResourceNotFoundException;
 import com.java_todo.springbootpgreacttodo.model.ToDo;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1")
 public class ToDoController {
