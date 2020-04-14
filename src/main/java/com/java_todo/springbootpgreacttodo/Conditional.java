c0838f96da98a8ff6b67f689c74406bd13ccf325
Allen Lucke <allenlucke@gmail.com>
2020-04-14 15:25:25 -0500

    moved conditionals

---
+++ src/main/java/com/java_todo/springbootpgreacttodo/Conditional.java
@@ -0,0 +1,25 @@
+//import java.io.Console;
+////
+////public class Conditional {
+////
+////    public static void main(String[] args) {
+////        Console console = System.console();
+////
+////        String ageAsString = console.readLine("How old are you?  ");
+////
+////        int age = Integer.parseInt(ageAsString);
+////
+////        if(age < 18) {
+////            console.printf("Your table is ready\n");
+////            System.exit(0);
+////        }
+////        String noun = console.readLine("Enter a noun:  ");
+////
+////        if(noun.equalsIgnoreCase("dork") ||
+////                noun.equalsIgnoreCase("jerk") ||
+////                noun.equalsIgnoreCase("pinhead")) {
+////            console.printf("you must choose another noun");
+////            System.exit(0);
+////        }
+////    }
+////}
